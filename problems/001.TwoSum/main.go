417edc27ee54a2466c9d1953e399b6f024b5ccd2
Denis Shashkov <dshashkov@beget.ru>
2021-08-19 17:55:06 +0300

    fast

--- problems/001.TwoSum/main.go
+++ problems/001.TwoSum/main.go
@@ -4,7 +4,24 @@ import "fmt"
 
 func main()  {
 	fmt.Println(twoSumLoops([]int{2,7,11,15}, 9))
+	fmt.Println(twoSum([]int{2,7,11,15}, 9))
 }
+
+// Fast solution for example
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums)) // memory save
+
+	for i, v := range nums {
+		if idx, ok := m[target - v]; ok {
+			return []int{idx, i}
+		}
+		m[v] = i
+	}
+
+	return nil // []int{}
+}
+
+
 // Slow solution for example
 func twoSumLoops(nums []int, target int) []int {
 	for i:= 0; i < len(nums); i++ {
