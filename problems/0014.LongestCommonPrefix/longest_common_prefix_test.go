b55ea93ac263c0e17e6e934253073588d66008cd
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-09-02 23:36:46 +0300

    bench14

--- problems/0014.LongestCommonPrefix/longest_common_prefix_test.go
+++ problems/0014.LongestCommonPrefix/longest_common_prefix_test.go
@@ -31,3 +31,28 @@ func TestLongestCommonPrefix(t *testing.T) {
 		}
 	}
 }
+
+func TestLongestCommonPrefix2(t *testing.T) {
+	for _, v := range getTestcases() {
+		output := longestCommonPrefix2(v.input)
+		if !reflect.DeepEqual(output, v.expected) {
+			t.Errorf("Expect %v: got %v", v.expected, output)
+		}
+	}
+}
+
+func BenchmarkLongestCommonPrefix(b *testing.B)  {
+	test := getTestcases()[2]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		longestCommonPrefix(test.input)
+	}
+}
+
+func BenchmarkLongestCommonPrefix2(b *testing.B)  {
+	test := getTestcases()[2]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		longestCommonPrefix2(test.input)
+	}
+}
