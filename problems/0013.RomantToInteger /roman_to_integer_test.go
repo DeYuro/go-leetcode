03c44aa212701190d86da3002cce1da180d0a5b5
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-09-07 21:58:08 +0300

    problem13

---
+++ problems/0013.RomantToInteger /roman_to_integer_test.go
@@ -0,0 +1,43 @@
+package problem13
+
+import (
+	"reflect"
+	"testing"
+)
+
+type test struct {
+	input    string
+	expected int
+}
+
+func getTestcases() []test {
+	return []test{
+		{
+			"III",
+			3,
+		},
+		{
+			"IV",
+			4,
+		}, {
+			"IX",
+			9,
+		}, {
+			"LVIII",
+			58,
+		},
+		{
+			"MCMXCIV",
+			1994,
+		},
+	}
+}
+
+func TestRomanToInt(t *testing.T) {
+	for _, v := range getTestcases() {
+		output := romanToInt(v.input)
+		if !reflect.DeepEqual(output, v.expected) {
+			t.Errorf("Expect %v: got %v", v.expected, output)
+		}
+	}
+}
