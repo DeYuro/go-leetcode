1e7e6cb962aef82ed4b59ee5183ae1314e40984c
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-09-07 22:10:52 +0300

    fix problem13

--- problems/0013.RomantToInteger/roman_to_Integer.go
+++ problems/0013.RomantToInteger/roman_to_Integer.go
@@ -15,22 +15,25 @@ func romanToInt(s string) int {
 	for i := 0; i < len(s); i++ {
 		if i == 0 {
 			dec =+ roman[s[i]]
-			continue;
+			continue
 		}
 		//I can be placed before V (5) and X (10) to make 4 and 9.
 		//X can be placed before L (50) and C (100) to make 40 and 90.
 		//C can be placed before D (500) and M (1000) to make 400 and 900.
-		if s[i] == 'V' || s[i] == 'X' && s[i-1] == 'I' {
-			dec+=roman[s[i]]
+		if (s[i] == 'V' || s[i] == 'X') && s[i-1] == 'I' {
+			dec+=roman[s[i]]-roman[s[i-1]]-1
+			continue
 		}
-		if s[i] == 'L' || s[i] == 'C' && s[i-1] == 'X' {
-			dec+=roman[s[i]]-10
+		if (s[i] == 'L' || s[i] == 'C') && s[i-1] == 'X' {
+			dec+=roman[s[i]]-roman[s[i-1]]-10
+			continue
 		}
 
-		if s[i] == 'C' || s[i] == 'M' && s[i-1] == 'C' {
-			dec+=roman[s[i]]-100
+		if (s[i] == 'D' || s[i] == 'M') && s[i-1] == 'C' {
+			dec+=roman[s[i]]-roman[s[i-1]]-100
+			continue
 		}
-		dec=+roman[s[i]]
+		dec+=roman[s[i]]
 	}
 
 	return dec
