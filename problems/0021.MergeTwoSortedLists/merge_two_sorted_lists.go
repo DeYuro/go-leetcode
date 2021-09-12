a681cb9fafdf37f1bd1b2ede09ae5c964d0b1af0
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-09-13 00:51:04 +0300

    problem21 slow

--- problems/0021.MergeTwoSortedLists/merge_two_sorted_lists.go
+++ problems/0021.MergeTwoSortedLists/merge_two_sorted_lists.go
@@ -1,21 +1,33 @@
 package problem21
 
-import "github.com/go-leetcode/structures"
+import (
+	"github.com/go-leetcode/structures"
+	"sort"
+)
 
 func mergeTwoLists(l1 *structures.ListNode,l2 *structures.ListNode) *structures.ListNode {
-	result := &structures.ListNode{}
-
-
-	for l1.Val != nil && l2.Val != nil {
-		if l1 != nil {
-			val1 := l1.Val
-		}
-
-		if l2.Val != nil {
-			val2 := l2.Val
-		}
+	sorted := []int{}
+	merged := &structures.ListNode{}
+	head := merged
+	for l1 != nil {
+		sorted = append(sorted, l1.Val)
+		l1 = l1.Next
 	}
 
+	for l2 != nil {
+		sorted = append(sorted, l2.Val)
+		l2 = l2.Next
+	}
 
-	return result
+	sort.Ints(sorted)
+
+	for _,v := range sorted{
+		merged.Next = &structures.ListNode{
+			Val:  v,
+			Next: nil,
+		}
+		merged = merged.Next
+	}
+
+	return head.Next
 }
