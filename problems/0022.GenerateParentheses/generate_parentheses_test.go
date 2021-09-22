0e8021c6ff6e27c1adf6d8fe3eb0a39c339babff
dshashkov <asushashkov@gmail.com>
2021-09-23 00:11:11 +0300

    problem22 comment and test

--- problems/0022.GenerateParentheses/generate_parentheses_test.go
+++ problems/0022.GenerateParentheses/generate_parentheses_test.go
@@ -13,10 +13,10 @@ type test struct {
 
 func getTestcases() []test {
 	return []test{
-		//{
-		//	4,
-		//	[]string{"((()))","(()())","(())()","()(())","()()()"},
-		//},
+		{
+			3,
+			[]string{"((()))","(()())","(())()","()(())","()()()"},
+		},
 		{
 			2,
 			[]string{"(())","()()"},
@@ -34,5 +34,13 @@ func TestGenerateParenthesis(t *testing.T) {
 		if !reflect.DeepEqual(output, v.expected) {
 			t.Errorf("Expect %v: got %v", v.expected, output)
 		}
+	}
+}
+
+func BenchmarkGenerateParenthesis(b *testing.B) {
+	test := getTestcases()[0]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		generateParenthesis(test.input)
 	}
 }
