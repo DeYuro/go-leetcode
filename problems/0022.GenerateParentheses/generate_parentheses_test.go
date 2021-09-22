3632a1a47375d8ddb984142a8aab332b8d837f89
dshashkov <asushashkov@gmail.com>
2021-09-22 23:59:15 +0300

    problem22

--- problems/0022.GenerateParentheses/generate_parentheses_test.go
+++ problems/0022.GenerateParentheses/generate_parentheses_test.go
@@ -1,1 +1,38 @@
 package problem22
+
+import (
+	"reflect"
+	"testing"
+)
+
+type test struct {
+	input    int
+	expected []string
+}
+
+
+func getTestcases() []test {
+	return []test{
+		//{
+		//	4,
+		//	[]string{"((()))","(()())","(())()","()(())","()()()"},
+		//},
+		{
+			2,
+			[]string{"(())","()()"},
+		},
+		{
+			1,
+			[]string{"()"},
+		},
+	}
+}
+
+func TestGenerateParenthesis(t *testing.T) {
+	for _, v := range getTestcases() {
+		output := generateParenthesis(v.input)
+		if !reflect.DeepEqual(output, v.expected) {
+			t.Errorf("Expect %v: got %v", v.expected, output)
+		}
+	}
+}
