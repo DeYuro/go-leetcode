dca0e39d712a9ec27e661227455eb9690e1f0b1f
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-10-03 21:51:00 +0300

    time limit exc

--- problems/0053.MaximumSubarray/maximum_subarray.go
+++ problems/0053.MaximumSubarray/maximum_subarray.go
@@ -1,6 +1,26 @@
 package problem53
 
+import "math"
+// correct but Time Limit Exceeded
 func maxSubArray(nums []int) int {
-	var max int
+	max := math.MinInt64
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	for i := 0; i < len(nums); i++ {
+		localMax := nums[i]
+
+		for j := i+1; j < len(nums); j++ {
+			localMax += nums[j]
+			if localMax > max {
+				max = localMax
+			}
+		}
+		if localMax > max {
+			max = localMax
+		}
+	}
+
 	return max
 }
