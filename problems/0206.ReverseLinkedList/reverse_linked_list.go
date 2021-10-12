974ffa7c4eb332448e59cda1a5d0aa173491b6ea
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-10-12 23:48:21 +0300

    problem206

---
+++ problems/0206.ReverseLinkedList/reverse_linked_list.go
@@ -0,0 +1,42 @@
+package problem206
+
+import "github.com/go-leetcode/structures"
+
+func reverseList(head *structures.ListNode) *structures.ListNode {
+	var cur, next, prev *structures.ListNode
+	cur = head
+
+	for cur != nil {
+		//[1->2->3->4->5]
+
+		// [2->3->4->5->nil]
+		// [3->4->5->nil]
+		// [4->5->nil]
+		// [5->nil]
+		// [nil]
+		next = cur.Next
+
+		// [nil] on first
+		// [1->nil]
+		// [2->1->nil]
+		// [3->2->1->nil]
+		// [4->3->2->1->nil]
+		cur.Next = prev
+
+		// [1->nil]
+		// [2->1->nil]
+		// [3->2->1->nil]
+		// [4->3->2->1->nil]
+		// [5->4->3->2->1]  !!! prev is answer
+		prev = cur
+
+		// [2->3->4->5->nil]
+		// [3->4->5->nil]
+		// [4->5->nil]
+		// [5->nil]
+		// [nil] loop end
+		cur  = next
+	}
+
+	return prev
+}
