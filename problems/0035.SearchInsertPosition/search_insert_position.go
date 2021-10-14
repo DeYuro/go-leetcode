4cd8023ee9407fd8191dc593fb22c4bdf18f38ef
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-10-14 22:46:11 +0300

    problem66

--- problems/0035.SearchInsertPosition/search_insert_position.go
+++ problems/0035.SearchInsertPosition/search_insert_position.go
@@ -1,45 +1,5 @@
 package problem35
 
-//
-//func searchInsert(nums []int, target int) int {
-//	if nums[len(nums)/2] >= target {
-//		return helper(nums, len(nums)/2, len(nums) -1, target)
-//	} else {
-//		return helper(nums, 0, len(nums)/2, target)
-//	}
-//}
-////[1,2,3,4,5,6,7,8,9,10]
-//
-//// t = 8
-////[1,2,3,6,7,8,9]
-//// [8,9]
-//func helper(nums []int, l,r,t int) int {
-//	if t == nums[l] {
-//		return l
-//	}
-//
-//	if t == nums[r] {
-//		return r
-//	}
-//
-//	if l == 0 {
-//		return 0
-//	}
-//
-//	if l >= len(nums)-1 {
-//		return len(nums) - 1
-//	}
-//
-//	if t > l
-//	//if l == r {
-//	//	if t > l {
-//	//		return l + 1
-//	//	} else
-//	//}
-//
-//	return helper(nums, l, r, t)
-//}
-
 func searchInsert(nums []int, target int) int {
 	if target > nums[len(nums)-1] {
 		return len(nums)
