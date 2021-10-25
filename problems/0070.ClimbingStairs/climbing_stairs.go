ab4375f4188e2078f32681a5737e6d65b13e601f
Денис Шашков <deyurosh@gmail.com>
2021-10-25 22:12:08 +0300

    fibonachi

--- problems/0070.ClimbingStairs/climbing_stairs.go
+++ problems/0070.ClimbingStairs/climbing_stairs.go
@@ -1,6 +1,7 @@
 package problem70
 
-func climbStairs(n int) int {
+
+func climbStairsDp(n int) int {
 	dp := make([]int, n+1)
 	dp[0] = 1
 	dp[1] = 1
@@ -8,4 +9,20 @@ func climbStairs(n int) int {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
 	return dp[n]
+}
+
+func climbStairsFib(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	a,b := 1, 2
+
+	for i := 3; i <= n; i++ {
+		save := b
+		b = a + b
+		a = save
+	}
+
+	return b
 }
