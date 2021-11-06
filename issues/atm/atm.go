67983a9d8ed86790f99d26eb1345005c0a47fc74
Денис Шашков <deyurosh@gmail.com>
2021-11-06 23:56:49 +0300

    refactor tests

--- issues/atm/atm.go
+++ issues/atm/atm.go
@@ -4,87 +4,7 @@ import (
 	"sort"
 )
 
-func atm(amount int, bills map[int]int) []int {
-
-	r := 1
-	//save := amount
-	var change []int
-	var sorted []int
-
-	for i := range bills {
-		sorted = append(sorted, i)
-	}
-
-	sort.Sort(sort.Reverse(sort.IntSlice(sorted)))
-	startPos := 0
-
-	pos := 0
-
-	for amount > 0 {
-		i := pos
-		for ;i < len(sorted); {
-			if bills[sorted[i]] < 1 { //if  bills[i] not enough go to next bill
-				i++
-				continue
-			}
-
-			res := amount - sorted[i] // result  = amount - current bill value
-
-			if res < 0 {
-				// if result less then 0 or bills not enough go to next bill
-				i++
-				continue
-			}
-
-			amount = res // amount decrease
-			bills[sorted[i]]--  // decrease bills
-			pos = startPos // reset position
-
-			change = append(change, sorted[i])
-			continue
-		}
-
-		if pos < len(sorted) && len(change) > 0 {
-			if amount == 0 {
-				return change
-			}
-
-			for j := 1; j <= r; j++ {
-				amount += change[len(change)-j]
-
-			}
-
-
-			//v := change[len(change)-1]
-			//_ = v
-			amount += change[len(change)-1]
-			//amount = save
-
-			//return bills to atm
-			for _,v := range change {
-				bills[v]++
-			}
-
-			bills[change[len(change)-1]]++
-			change = []int{}
-			//change = change[:len(change)-1]
-
-
-			startPos = min(len(sorted)-1, startPos + 1)
-
-			pos = startPos
-		}
-
-		if i > len(sorted) {
-			return []int{}
-		}
-	}
-
-
-	return change
-}
-
-func min(a,b int) int {
+func min(a, b int) int {
 	if a < b {
 		return a
 	}
@@ -92,21 +12,53 @@ func min(a,b int) int {
 	return b
 }
 
-func atm2()  {
-	
+func atm(amount int, bills map[int]int) []int {
+
+	noms := sortedNoms(bills)
+	test := collect(amount, noms, bills)
+	_ = test
+	return []int{}
 }
 
-func collect(amount int, noms []int) []int {
+func collect(amount int, noms []int, bills map[int]int) map[int]int {
 	if amount == 0 {
-		return []int{}
+		return map[int]int{}
 	}
 
 	if len(noms) == 0 {
-		return []int{}
+		return map[int]int{}
+	}
+	curNom := noms[0]
+	available := bills[curNom]
+	need := amount / curNom
+	number := min(available, need)
+	for i := number; i >= 0; i-- {
+		result := collect2(amount-i*curNom, noms[1:], bills)
+
+		if len(result) > 0 {
+			if i > 0 {
+				m := map[int]int{curNom: i}
+				for k, v := range result {
+					m[k] = v
+				}
+				return m
+			} else {
+				return result
+			}
+		}
+	}
+	return nil
+}
+
+
+func sortedNoms(bills map[int]int) []int {
+	var sorted []int
+
+	for i := range bills {
+		sorted = append(sorted, i)
 	}
 
-	curNom := noms[0]
-	numsNotes := amount / curNom
+	sort.Sort(sort.Reverse(sort.IntSlice(sorted)))
 
-
+	return sorted
 }
