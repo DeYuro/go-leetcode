79f22d308cea0b5e2fc3b9a21249a37e5585ff5f
Denis Shashkov <dshashkov@beget.ru>
2021-12-31 21:00:37 +0300

    init 136

---
+++ problems/0136.SingleNumber/single_number.go
@@ -0,0 +1,21 @@
+package problem136
+
+func singleNumber(nums []int) int {
+
+	var a int
+	for i, v := range nums {
+		if i == 0 {
+			a = v
+			continue
+		}
+
+		if v == a {
+
+		}
+
+
+	}
+
+	return a
+}
+
---
+++ problems/0136.SingleNumber/single_number_test.go
@@ -0,0 +1,38 @@
+package problem136
+
+import (
+	"reflect"
+	"testing"
+)
+
+type test struct {
+	input     []int
+	expected int
+}
+
+func getTestcases() []test {
+	return []test{
+		{
+			[]int{2,2,1},
+			1,
+		},
+		{
+			[]int{4,1,2,1,2},
+			4,
+		},
+		{
+			[]int{1},
+			1,
+		},
+
+	}
+}
+
+func TestSingleNumber(t *testing.T) {
+	for _, v := range getTestcases() {
+		output := singleNumber(v.input,)
+		if !reflect.DeepEqual(output, v.expected) {
+			t.Errorf("Expect %v: got %v", v.expected, output)
+		}
+	}
+}
