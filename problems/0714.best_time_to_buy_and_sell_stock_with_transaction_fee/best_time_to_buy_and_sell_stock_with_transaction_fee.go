6490ee30c10fc235faa29c9000cd758f3d222900
Denis Shashkov <dshashkov@beget.ru>
2022-02-15 16:44:18 +0300

    problem714

---
+++ problems/0714.best_time_to_buy_and_sell_stock_with_transaction_fee/best_time_to_buy_and_sell_stock_with_transaction_fee.go
@@ -0,0 +1,52 @@
+package problem714
+
+import "math"
+
+func maxProfit(prices []int) int {
+	sold := 0
+	rest := 0
+	hold := math.MinInt32
+	for i :=0 ;i < len(prices); i++{
+		prevSold := sold
+		sold = hold + prices[i]
+
+		rmp := rest - prices[i]
+
+		hold = max(hold, rmp)
+		rest = max(rest, prevSold)
+	}
+	return max(rest, sold)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+
+func maxProfit2(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	dp := make([]int, len(prices))
+	ans := 0
+
+	for i := 0; i < len(prices); i++ {
+		maxProfit := 0
+		for j := i - 1; j >= 0; j-- {
+			maxProfit = max(maxProfit, prices[i]-prices[j])
+			d := 0
+			if j-2 >= 0 {
+				d = dp[j-2]
+			}
+			dp[i] = max(dp[i], d+maxProfit)
+		}
+
+		ans = max(ans, dp[i])
+	}
+
+	return ans
+}
